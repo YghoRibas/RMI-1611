586baf331600e2b11cc406766e36d0bb34b7232b
Yghor Gomes <55198336+YghoRibas@users.noreply.github.com>
2023-11-26 11:14:53 -0300

    Create ICalculadora.java

---
+++ ICalculadora.java
@@ -0,0 +1,13 @@
+public interface Calculadora {
+    // Método para realizar a adição
+    double adicao(double num1, double num2);
+
+    // Método para realizar a subtração
+    double subtracao(double num1, double num2);
+
+    // Método para realizar a multiplicação
+    double multiplicacao(double num1, double num2);
+
+    // Método para realizar a divisão
+    double divisao(double num1, double num2);
+}
